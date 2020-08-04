0ccb6a150b3f184ddeb4a9d130a611023ca489e7
lnkz80 <lnkz80@gmail.com>
2020-08-04 16:59:26 +0300

    homework

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-const modal = $.modal();
+const options = {
+  objClass: "info",
+  title: "Hello!",
+  closable: true,
+  content:
+    "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>",
+  width: "400px",
+};
+
+const modal = $.modal(options);
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,19 +1,19 @@
 function _createModal(options) {
   const modal = document.createElement("div");
   modal.classList.add("amodal");
+  options.closable
+    ? (x = '<span class="modal-close">&times;</span>')
+    : (x = "");
   modal.insertAdjacentHTML(
     "afterbegin",
     `<div class="modal-overlay">
       <div class="modal-window">
         <div class="modal-header">
-          <span class="modal-title">Modal title</span>
-          <span class="modal-close">&times;</span>
+          <span class="modal-title">${options.title}</span>
+          ${x}
         </div>
         <div class="modal-body">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+          ${options.content}
         </div>
         <div class="modal-footer">
           <button>Ok</button>
@@ -22,7 +22,9 @@ function _createModal(options) {
       </div>
     </div>`
   );
+
   document.body.appendChild(modal);
+  document.querySelector(".modal-window").style.width = options.width;
   return modal;
 }
 
@@ -30,19 +32,32 @@ $.modal = function (options) {
   const ANIMATION_SPEED = 200;
   const $modal = _createModal(options);
   let closing = false;
+  const elClose = document.querySelector(".modal-close");
+  const elOverlay = document.querySelector(".modal-overlay");
+  const objClass = "." + options.objClass;
+  open = () => {
+    !closing && $modal.classList.add("open");
+    elClose.onclick = close;
+    elOverlay.onclick = close;
+  };
+  close = () => {
+    closing = true;
+    $modal.classList.remove("open");
+    $modal.classList.add("hide");
+    setTimeout(() => {
+      $modal.classList.remove("hide");
+      closing = false;
+    }, ANIMATION_SPEED);
+
+    elClose.removeEventListener("click");
+    elOverlay.removeEventListener("click");
+  };
+
+  document.querySelector(objClass).onclick = open;
+
+  document.querySelector(objClass).removeEventListener("click");
+
   return {
-    open() {
-      !closing && $modal.classList.add("open");
-    },
-    close() {
-      closing = true;
-      $modal.classList.remove("open");
-      $modal.classList.add("hide");
-      setTimeout(() => {
-        $modal.classList.remove("hide");
-        closing = false;
-      }, ANIMATION_SPEED);
-    },
     destroy() {},
   };
 };
