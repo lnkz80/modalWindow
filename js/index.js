5a8863a28f4e6605dcd2efbd6b17a40963bba3a1
lnkz80 <lnkz80@gmail.com>
2020-10-01 08:17:39 +0300

    01102020_home

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 const cards = [
   {
     id: 0,
-    imgsrc: "img/maserati.png",
-    title: "Maserati",
+    imgsrc: 'img/maserati.png',
+    title: 'Maserati',
     text:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur     quas, velit molestias quasi deserunt iste.",
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur     quas, velit molestias quasi deserunt iste.',
     price: 50000,
   },
   {
     id: 1,
-    imgsrc: "img/mustang.png",
-    title: "Ford",
+    imgsrc: 'img/mustang.png',
+    title: 'Ford',
     text:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur     quas, velit molestias quasi deserunt iste.",
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur     quas, velit molestias quasi deserunt iste.',
     price: 40000,
   },
   {
     id: 2,
-    imgsrc: "img/porsche.png",
-    title: "Porche",
+    imgsrc: 'img/porsche.png',
+    title: 'Porche',
     text:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur     quas, velit molestias quasi deserunt iste.",
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur     quas, velit molestias quasi deserunt iste.',
     price: 90000,
   },
 ];
@@ -33,7 +33,7 @@ toHTML = (card) => `
             <h5 class="card-title">${card.title}</h5>
             <p class="card-text">${card.text}</p>
             <div class="d-flex justify-content-center">
-                <a href="#" class="btn btn-sm btn-primary" data-btn="price" id="${card.id}">See price</a>&nbsp;
+                <a href="#" class="btn btn-sm btn-primary" data-btn="price" data-id="${card.id}">See price</a>&nbsp;
                 <a href="#" class="btn btn-sm btn-danger" data-btn="delete">Delete</a>
             </div>
         </div>
@@ -50,20 +50,20 @@ function render() {
   //   document.querySelector("#cards").innerHTML = html;
   //==================================================
   //ALL IN ONE STRING
-  document.querySelector("#cards").innerHTML = cards.map(toHTML).join("");
+  document.querySelector('#cards').innerHTML = cards.map(toHTML).join('');
 }
 
 render();
 
 const priceModal = $.modal({
-  title: "The price is",
+  title: 'The price is',
   closable: true,
-  width: "300px",
+  width: '300px',
   buttons: [
     {
-      text: "Close",
-      style: "primary",
-      size: "sm",
+      text: 'Close',
+      style: 'primary',
+      size: 'sm',
       handler() {
         priceModal.close();
       },
@@ -71,14 +71,17 @@ const priceModal = $.modal({
   ],
 });
 
-document.addEventListener("click", (event) => {
+document.addEventListener('click', (event) => {
   event.preventDefault();
   const btnType = event.target.dataset.btn;
-  const id = +event.target.dataset.id;
+  const id = +event.target.dataset.id; //плюс перед значением переводит его в числовое значение
 
-  if (btnType === "price") {
-    const card = cards.find((c) => c.id === id);
+  if (btnType === 'price') {
+    const card = cards.find((item) => item.id === id);
+    priceModal.setContent(`
+    <p>Price for ${card.title} is $<strong>${card.price}</strong></p>
+    `);
     priceModal.open();
-    console.log(card);
+    // console.log(card);
   }
 });
